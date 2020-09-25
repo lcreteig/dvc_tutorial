cc9bb9f9428e4242dfcede8dad7a92d5acffa18b
Leon Reteig <leonreteig@gmail.com>
2020-09-25 14:37:05 +0200

    modify code and thus data

--- code.py
+++ code.py
@@ -2,6 +2,6 @@ import pandas as pd
 data = {'Name':['Tom', 'Jack', 'nick', 'juli'], 
 'Age':[40, 11, 78, 28]} 
 df = pd.DataFrame(data=data) 
+df['Hobby'] = ['Football', 'Video Games', 'Reading', 'Running'] 
 print(df) 
-# df['Hobby'] = ['Football', 'Video Games', 'Reading', 'Running'] 
 df.to_csv('data/df.csv')
